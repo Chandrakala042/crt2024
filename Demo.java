9a817f8b106010b1c71a99761016bc8a6f7b020e
Chandrakala042 <pemirachandrakala042@gmail.com>
2024-06-25 10:40:26 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Demo
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int m,n;
+m=s.nextInt();
+n=s.nextInt();
+int b=(int)Math.pow(m,n);
+System.out.println(b);
+
+
+
+}
+}
